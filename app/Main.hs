feb7a7954cb30cae68d17a5873985b73fea2a00f
Christoffer Lind <48015997+Dekkon@users.noreply.github.com>
2024-06-14 17:36:01 +0200

    can now run programs

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,41 @@
+-- This module defines a simple command line interface for the Boa
+-- interpreter.  If your solution is correct, this module should just
+-- work.
 module Main (main) where
 
+import AST
+import Parser
+import Interpreter
+
+import System.Exit (die)
+import System.Environment (getArgs)
+import qualified Data.Map.Strict as M
+import Data.Map(Map)
+
+run :: Program -> IO ()
+run p =
+  do let res = executeProgram p
+    --  mapM_ putStrLn out
+     case res of
+       Left e -> print e
+       Right env -> mapM_ print (M.toList env)
 
 main :: IO ()
-main = undefined
+main = do args <- getArgs
+          case args of
+            -- ["-i", file] -> do
+            --   s <- readFile file
+            --   run $ read s
+            -- ["-p", file] -> do
+            --   s <- readFile file
+            --   case parseString s of
+            --     Left e -> putStrLn $ "*** Parse error: " ++ show e
+            --     Right p -> putStrLn $ show p
+            [file] -> do
+              s <- readFile file
+              case parseProgram s of
+                Left e -> putStrLn $ "*** Parse error: " ++ show e
+                Right p -> run p
+            _ ->
+              die "Usage:\n\
+                    \  janus PROGRAM.janus"
